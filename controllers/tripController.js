cdbbdaa15a42a3a32440ce5c1656acae2d948c81
karasev-a <karasev.avk@gmail.com>
2018-06-30 15:34:00 +0300

    create controller of trip and change index controller

---
+++ controllers/tripController.js
@@ -0,0 +1,72 @@
+const { Trip } = require('../models');
+
+module.exports = {
+  // Create and Save a new Trip
+  createTrip(req, res) {
+    return Trip.create({
+      name: req.body.name,
+      dateStart: req.body.dateStart,
+      dateEnd: req.body.dateEnd,
+      description: req.body.description,
+      idUser: req.body.idUser,
+    }).then(() => {
+      res.status(201);
+      res.redirect('/');
+    }).catch(error => res.status(400).send(error));
+  },
+
+  // Retrieve and return all trips from the database.
+  findAllTrips(req, res) {
+    return Trip.findAll().then((trips) => {
+      res.status(200);
+      res.json(trips);
+    }).catch(error => res.status(400).send(error));
+  },
+
+  // Find a single trip with a tripId
+  findTrip(req, res) {
+    return Trip.findById(req.params.id).then((trip) => {
+      if (!trip) {
+        return res.status(404).send({
+          message: `Trip with id ${req.params.id} not found`,
+        });
+      }
+      return res.status(200).json(trip);
+    }).catch(error => res.status(400).send(error));
+  },
+
+  // Update a trip identified by the tripId in the request
+  updateTrip(req, res) {
+    return Trip.findById(req.params.id).then((trip) => {
+      if (!trip) {
+        return res.status(404).send({
+          message: `Trip with id ${req.params.id} not found`,
+        });
+      }
+      return trip.update({
+        name: req.body.name,
+        dateStart: req.body.dateStart,
+        dateEnd: req.body.dateEnd,
+        description: req.body.description,
+        idUser: req.body.idUser,
+      }).then(() => {
+        res.status(200).send({ message: 'Trip updated successfully.' });
+      });
+    });
+  },
+
+  // Delete a trip with the specified tripId in the request
+  deleteTrip(req, res) {
+    return Trip.findById(req.params.id).then((trip) => {
+      if (!trip) {
+        return res.status(404).send({
+          message: `Trip with id ${req.params.id} not found`,
+        });
+      }
+      return trip.destroy().then(() => {
+        res.status(200);
+        res.send({ message: 'Trip deleted successfully.' });
+      });
+    });
+  },
+};
