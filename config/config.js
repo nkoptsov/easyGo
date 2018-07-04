885c465840688455618321eb0c004dca57da2bbb
nikita <>
2018-07-04 13:26:49 +0300

    add field password

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   development: {
     username: process.env.DB_USERNAME,
-    password: null,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_DEVELOPMENT,
     host: process.env.DB_HOSTNAME,
     dialect: 'postgres',
