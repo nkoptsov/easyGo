e636b69e87e96a5ed0d41775568c8f27f5533299
Julisitta <37798545+Julisitta@users.noreply.github.com>
2018-07-05 11:22:26 +0300

    edd validate

--- migrations/20180626085201-create-trip.js
+++ migrations/20180626085201-create-trip.js
@@ -9,27 +9,64 @@ module.exports = {
     },
     name: {
       type: Sequelize.STRING,
+      validate: {
+        is: /^[a-z]+$/i,
+        len: [5,15],
+        notEmpty: true,
+      }
     },
     dateStart: {
       type: Sequelize.DATEONLY,
+      validate: {
+        isDate: true,
+        isBefore: dateEnd,
+        isAfter: time.Now(),
+        notNull: true,
+      }
     },
     dateEnd: {
       type: Sequelize.DATEONLY,
+      validate: {
+        isDate: true,
+        isAfter: dateStart,
+        notNull: true,
+      }
     },
     locationStart: {
       type: Sequelize.STRING,
+      validate: {
+        is: /^[a-z]+$/i,
+        len: [5,15],
+        notEmpty: true,
+      }
     },
     locationEnd: {
       type: Sequelize.STRING,
+      validate: {
+        is: /^[a-z]+$/i,
+        len: [5,15],
+        notEmpty: true,
+      }
     },
     tripCost: {
       type: Sequelize.DOUBLE,
+      validate: {
+        isDecimal: true,
+        notNull: true,
+      }
     },
     description: {
       type: Sequelize.TEXT,
+      validate: {
+        isNull: true,
+      }
     },
     userId: {
       type: Sequelize.INTEGER,
+      validate: {
+        notNull: true,
+        isInt: true,
+      }
     },
     createdAt: {
       allowNull: false,
