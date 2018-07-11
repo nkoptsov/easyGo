bf3ba311c07588700c946f22044e13135905c785
karasev-a <karasev.avk@gmail.com>
2018-07-11 09:33:14 +0300

    fix bugs

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
 const userRouter = require('./userRouter');
+const profileRouter = require('./profileRouter');
 const tripRouter = require('./tripRoutes');
 
 const router = express.Router();
 
 router.use('/users', userRouter);
+router.use('/users', profileRouter);
 router.use('/', tripRouter);
 
 router.get('/', (req, res) => {
