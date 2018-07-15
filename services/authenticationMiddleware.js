04e247da6e7240160185ef0161a12897a1041eeb
nikita <nikita@MacBook-Pro-nikita.local>
2018-07-15 20:03:09 +0300

    add in view/register some fields

--- services/authenticationMiddleware.js
+++ services/authenticationMiddleware.js
@@ -2,5 +2,5 @@ module.exports = function authenticationMiddleware(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
-  res.redirect(401, '/users/login');
+  return res.redirect(401, '/users/login');
 };
