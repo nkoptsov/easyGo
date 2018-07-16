260227904a62acfe3ab6fabf8c3eee9e84a88435
nkoptsov <nkoptsov@gmail.com>
2018-07-16 18:28:23 +0300

    refactoring service

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,29 +5,15 @@ module.exports = {
   create(req, res) {
     const { body } = req;
     const result = createUserProfile(body);
-    result()
+
+    result
       .then((value) => {
         console.log(value);
         res.status(200).send();
       })
-      .catch((err) => {
-        console.log(err);
+      .catch((error) => {
+        console.log(error);
+        res.status(404).send();
       });
-    // User
-    //   .create({
-    //     login: req.body.login,
-    //     password: User.generateHash(req.body.password),
-    //   })
-    //   .then((user) => {
-    //     Profile.create({
-    //       userId: user.id,
-    //       email: req.body.email,
-    //       firstName: req.body.firstName,
-    //       lastName: req.body.lastName,
-    //       phoneNumber: req.body.phoneNumber,
-    //     })
-    //       .then(res.status(200).json(user));
-    //   })
-    //   .catch(error => res.status(400).json({ message: `${error.message}` }));
   },
 };
--- services/userService.js
+++ services/userService.js
@@ -2,7 +2,7 @@ const { User, Profile, sequelize } = require('../models');
 
 module.exports = {
   createUserProfile(reqBody) {
-    return new Promise(() => {
+    return new Promise((res, rej) => {
       sequelize.transaction(tr => (
         User.create({
           login: reqBody.login,
@@ -12,14 +12,8 @@ module.exports = {
             reqBody.userId = createdUser.id;
             return Profile.create(reqBody, { transaction: tr });
           })
-      )).then(ceatedObject => {
-        console.log(ceatedObject);
-        console.log('yes');
-      })
-        .catch(err => {
-          console.log(err);
-          console.log('fuck you');
-        });
+      )).then(ceatedObject => res(ceatedObject))
+        .catch(err => rej(err));
     });
   },
 };
