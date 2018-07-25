ce7f8f2d141c2c79cd4bda13e4f5499f8b169614
karasev-a <karasev.avk@gmail.com>
2018-07-25 14:00:29 +0300

    client with only index and app

--- src/client/App.js
+++ src/client/App.js
@@ -1,25 +1,15 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-import TripsList from './Pages/TripsList';
-import Trip from './Pages/Trip';
-import Toolbar from './Components/Toolbar'
-import Content from './Components/Content';
-import Home from './Pages/Home';
+
 
 class App extends Component {
   render() {
     return (
-      <Router>
+      
         <div>
-          <Toolbar />
-          <Content>
-            <Route exact path="/" component={Home}/>
-            <Route  path="/trips/:tripId" component={Trip}/>
-            <Route exact  path="/trips" component={TripsList}/>
-          </Content>
+          <h1>EASY GO</h1>
         </div>
-      </Router>
+      
     );
   }
 }
