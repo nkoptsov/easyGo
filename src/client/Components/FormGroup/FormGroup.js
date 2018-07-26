02537c7606046b01be22d4f95a64b8927bf5d0cc
ak1nak <sergeycerb@gmail.com>
2018-07-26 14:03:24 +0300

    reworked FormGroup, RegisterForm, and tried fix babel spread

--- src/client/Components/FormGroup/FormGroup.js
+++ src/client/Components/FormGroup/FormGroup.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const FormGroup = (props) => (
   <div className="form-group">
-    <label htmlFor={props.for}>{props.name}</label>
-    <input type={props.type} className="form-control" id={props.id} placeholder={props.placeholder} />
+    <label htmlFor={props.for}>{props.label}</label>
+    <input type={props.type} className="form-control" id={props.id} placeholder={props.placeholder} name={props.name} value={props.value} onChange={props.onChange}/>
   </div>
 )
 
