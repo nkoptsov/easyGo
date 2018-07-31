5e672baea9b363ba8027095bdb79f20917718620
Natalia Podolyaka <natalia.podolyaka@ukr.net>
2018-07-31 22:03:15 +0300

    Merge branch 'dev' of C:\Users\DoctorComp\WebstormProjects\easyGo with conflicts.

---
+++ src/client/Containers/Trips/Subscriptions.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import Header from '../../Components/Header/Header';
+import TripsView from '../../Components/Trips/TripsView';
+
+class Subscriptions extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     data: [],
+  //   };
+  // }
+  //
+  // componentDidMount() {
+  //   fetch('/api/users/trips/subscribed',
+  //     {
+  //       method: 'POST',
+  //       credentials: 'include',
+  //     })
+  //     .then(res => res.json())
+  //     .then((res) => {
+  //       this.setState({ data: res });
+  //     })
+  //     .catch(err => console.log(`request failed ${err.message}`));
+  // }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <main>
+          {/*<TripsView data={this.state.data} />*/}
+        </main>
+      </div>
+    );
+  }
+}
+
+export default Subscriptions;
--- src/server/config/config.js
+++ src/server/config/config.js
@@ -3,21 +3,21 @@ require('dotenv').config();
 module.exports = {
   development: {
     username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    password: "1",
     database: process.env.DB_DEVELOPMENT,
     host: process.env.DB_HOSTNAME,
     dialect: 'postgres',
   },
   test: {
     username: 'root',
-    password: null,
+    password: "1",
     database: 'database_test',
     host: '127.0.0.1',
     dialect: 'postgres',
   },
   production: {
     username: 'root',
-    password: null,
+    password: "1",
     database: 'database_production',
     host: '127.0.0.1',
     dialect: 'postgres',
