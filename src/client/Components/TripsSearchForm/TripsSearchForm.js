39f27e63ff7d0790d1fe9d33a2b63744d9505c14
ak1nak <sergeycerb@gmail.com>
2018-08-01 12:17:18 +0300

    added collapse to TripsSearchForm

--- src/client/Components/TripsSearchForm/TripsSearchForm.js
+++ src/client/Components/TripsSearchForm/TripsSearchForm.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import FormGroup from '../FormGroup/FormGroup';
-import FormGroupDate from '../FormGroup/FormGroupDate';
 import ReactDOM from 'react-dom';
 import InputRange from 'react-input-range';
 import 'react-input-range/lib/css/index.css';
+import { Collapse, Button } from 'reactstrap';
+import FormGroupDate from '../FormGroup/FormGroupDate';
+import FormGroup from '../FormGroup/FormGroup';
 
 
 class TripsSearchForm extends Component {
@@ -17,12 +18,17 @@ class TripsSearchForm extends Component {
         dateEnd: '',
         locationStart: '',
         locationEnd: '',
-        userId: '',        
+        userId: '',
         description: '',
       },
-      tripCost: {min: 0, max: 1000},
+      tripCost: { min: 0, max: 1000 },
+      collapse: false
       errors: {},
     };
+  }
+
+  toggle = () => {
+    this.setState({ collapse: !this.state.collapse });
   }
 
   onChange = (e) => {
@@ -31,12 +37,12 @@ class TripsSearchForm extends Component {
 
   onSubmit = (e) => {
     let formData = '';
-    for (let key in this.state.data) {
+    for (const key in this.state.data) {
       if (this.state.data[key]) {
-        formData = formData + `${key}=${this.state.data[key]}&`;
+        formData += `${key}=${this.state.data[key]}&`;
       }
     }
-    formData = formData + `tripCost=${this.state.tripCost.min}-${this.state.tripCost.max}`
+    formData += `tripCost=${this.state.tripCost.min}-${this.state.tripCost.max}`;
     this.props.handleSearchSubmit(formData);
     e.preventDefault();
   }
@@ -47,17 +53,17 @@ class TripsSearchForm extends Component {
       <div className="container col-sm-6">
         <form className="searchForm" onSubmit={this.onSubmit}>
           <FormGroup for="name" type="text" id="name" placeholder="Enter trip name here.." name="name" label="Trip name" value={data.name} onChange={this.onChange} />
-          <FormGroupDate for="dateStart" label="Date trip starts" id="dateStart" name="dateStart" value={data.dateStart} onChange={this.onChange} />
-          <FormGroupDate for="dateEnd" label="Date trip ends" id="dateEnd" name="dateEnd" value={data.dateEnd} onChange={this.onChange} />
-          <FormGroup for="locationStart" type="text" id="locationStart" placeholder="Where trip starts?" name="locationStart" label="Start location" value={data.locationStart} onChange={this.onChange} />
-          <FormGroup for="locationEnd" type="text" id="locationEnd" placeholder="Where trip ends?" name="locationEnd" label="End location" value={data.locationEnd} onChange={this.onChange} />
-          <InputRange
-            maxValue={1000}
-            minValue={0}
-            value={this.state.tripCost}
-            formatLabel={tripCost => `${tripCost}$`}
-            onChange={tripCost => this.setState({ tripCost })} />
-          <button className="btn btn-primary">Search</button>
+          <InputRange maxValue={1000} minValue={0} value={this.state.tripCost} formatLabel={tripCost => `${tripCost}$`} onChange={tripCost => this.setState({ tripCost })} />
+          <Button color="primary" onClick={this.toggle} style={{ marginBottom: '1rem' }}>More options</Button>
+          <Collapse isOpen={this.state.collapse}>
+            <FormGroupDate for="dateStart" label="Date trip starts" id="dateStart" name="dateStart" value={data.dateStart} onChange={this.onChange} />
+            <FormGroupDate for="dateEnd" label="Date trip ends" id="dateEnd" name="dateEnd" value={data.dateEnd} onChange={this.onChange} />
+            <FormGroup for="locationStart" type="text" id="locationStart" placeholder="Where trip starts?" name="locationStart" label="Start location" value={data.locationStart} onChange={this.onChange} />
+            <FormGroup for="locationEnd" type="text" id="locationEnd" placeholder="Where trip ends?" name="locationEnd" label="End location" value={data.locationEnd} onChange={this.onChange} />
+          </Collapse>
+          <button className="btn btn-primary">
+            Search
+          </button>
         </form>
       </div>
     );
