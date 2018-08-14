dcf454a0d58d03ab358815708eeeaa19b0b0867c
karasev-a <karasev.avk@gmail.com>
2018-08-14 13:47:13 +0300

    fixed query to subscribet Trips on server

--- src/server/controllers/tripController.js
+++ src/server/controllers/tripController.js
@@ -178,7 +178,6 @@ module.exports = {
       attributes: [],
       include: [{
         model: Trip,
-        attributes: ['id', 'name', 'dateStart', 'dateEnd', 'locationStart', 'locationEnd', 'tripCost'],
       },
       ],
     })
