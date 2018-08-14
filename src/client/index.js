fb6c98b1c53a941a1dcfe495978647e7aa6888b4
karasev-a <karasev.avk@gmail.com>
2018-08-14 13:22:31 +0300

    fixed refresh page

--- src/client/index.js
+++ src/client/index.js
@@ -4,17 +4,35 @@ import './index.css';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
+
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer, applyMiddleware(thunkMiddleware));
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root'),
 );
